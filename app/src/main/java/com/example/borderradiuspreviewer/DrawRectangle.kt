061bfb492d13db83a5627363bc40742c6d25deef
kejlor <woj450@gmail.com>
2021-09-02 17:26:06 +0200

    Project still need a lot of work. Added edit text fields for each radius and button to draw rectangle.

--- app/src/main/java/com/example/borderradiuspreviewer/DrawRectangle.kt
+++ app/src/main/java/com/example/borderradiuspreviewer/DrawRectangle.kt
@@ -3,11 +3,13 @@ package com.example.borderradiuspreviewer
 import android.graphics.*
 import androidx.annotation.ColorInt
 
-class DrawRectangle(@ColorInt private val color: Int) : Painter {
-    private var topLeftRadius = 0f
-    private var topRightRadius = 0f
-    private var bottomLeftRadius = 0f
-    private var bottomRightRadius = 0f
+class DrawRectangle(
+    @ColorInt private val color: Int,
+    private var topLeftRadius: Float,
+    private var topRightRadius: Float,
+    private var bottomLeftRadius: Float,
+    private var bottomRightRadius: Float
+) : Painter {
     private val paint = Paint()
     private val path = Path()
     private val corners = floatArrayOf(
@@ -16,31 +18,17 @@ class DrawRectangle(@ColorInt private val color: Int) : Painter {
         bottomRightRadius, bottomRightRadius,     // Bottom right radius in px
         bottomLeftRadius, bottomLeftRadius      // Bottom left radius in px
     )
+
     override fun paint(canvas: Canvas) {
         drawBackground(canvas)
     }
+
     private fun drawBackground(canvas: Canvas) {
         paint.color = color
         val width = canvas.width.toFloat()
         val height = canvas.height.toFloat()
-        val rectF = RectF(width / 4 ,height / 4, width / 4 * 3, height / 4 * 3)
+        val rectF = RectF(width / 4, height / 4, width / 4 * 3, height / 4 * 3)
         path.addRoundRect(rectF, corners, Path.Direction.CW)
         canvas.drawPath(path, paint)
-    }
-    fun changeTopLeftRadius(radius: Float, canvas: Canvas) {
-        topLeftRadius = radius
-        drawBackground(canvas)
-    }
-    fun changeTopRightRadius(radius: Float, canvas: Canvas) {
-        topRightRadius = radius
-        drawBackground(canvas)
-    }
-    fun changeBottomRightRadius(radius: Float, canvas: Canvas) {
-        bottomRightRadius = radius
-        drawBackground(canvas)
-    }
-    fun changeBottomLeftRadius(radius: Float, canvas: Canvas) {
-        bottomLeftRadius = radius
-        drawBackground(canvas)
     }
 }
--- app/src/main/java/com/example/borderradiuspreviewer/MainActivity.kt
+++ app/src/main/java/com/example/borderradiuspreviewer/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.borderradiuspreviewer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import android.widget.FrameLayout
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 import com.example.borderradiuspreviewer.utils.resToPx
 import com.example.borderradiuspreviewer.utils.toColorInt
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.IllegalArgumentException
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,17 +25,51 @@ class MainActivity : AppCompatActivity() {
         val cardWidth = ViewGroup.LayoutParams.MATCH_PARENT
         val cardHeight = R.dimen.profile_card_height.resToPx(this).toInt()
         val profileCardContainer = findViewById<FrameLayout>(R.id.profileCardContainer)
-        val painter = DrawRectangle(
-            color = orangeColor
-        )
-        profileCardContainer.addView(
-            CustomPainter(
-                context = this,
-                width = cardWidth,
-                height = cardHeight,
-                painter = painter
+        var editTextTopLeftRadius = findViewById<EditText>(R.id.editTextTopLeftRadius)
+        var editTextTopRightRadius = findViewById<EditText>(R.id.editTextTopRightRadius)
+        var editTextBottomLeftRadius = findViewById<EditText>(R.id.editTextBottomLeftRadius)
+        var editTextBottomRightRadius = findViewById<EditText>(R.id.editTextBottomRightRadius)
+        val button = findViewById<Button>(R.id.buttonDraw)
+        var topLeftRadiusFromInput = 0f
+        var topRightRadiusFromInput = 0f
+        var bottomLeftRadiusFromInput = 0f
+        var bottomRightRadiusFromInput = 0f
+
+        editTextBottomLeftRadius.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                bottomLeftRadiusFromInput = editTextBottomLeftRadius.text.toString().toFloat()
+                val painter = DrawRectangle(
+                    color = orangeColor,
+                    topLeftRadius = topLeftRadiusFromInput,
+                    topRightRadius = topRightRadiusFromInput,
+                    bottomLeftRadius = editTextBottomLeftRadius.text.toString().toFloat(),
+                    bottomRightRadius = bottomRightRadiusFromInput
+                )
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+        })
+
+        button.setOnClickListener {
+            profileCardContainer.addView(
+                CustomPainter(
+                    context = this,
+                    width = cardWidth,
+                    height = cardHeight,
+                    painter = DrawRectangle(
+                        color = orangeColor, topLeftRadius = topLeftRadiusFromInput,
+                        topRightRadius = topRightRadiusFromInput,
+                        bottomLeftRadius = bottomLeftRadiusFromInput,
+                        bottomRightRadius = bottomRightRadiusFromInput
+                    )
+                )
             )
-        )
-//        TODO: need to implement EditText fields which will allow user to change each corner radius
+        }
     }
 }
